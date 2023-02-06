c04f518f8118ada3710aca4c807024e909537c6c
zxlim92 <55634410+zxlim92@users.noreply.github.com>
2023-02-06 00:00:22 -0700

    Stop tracking config.py

--- test/test.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-def get_url():
-  url = "mongodb+srv://zxlim:Mongo1234@cluster0.1p0y0.mongodb.net/?retryWrites=true&w=majority"
-  return url
-client = pymongo.MongoClient(get_url())
-print(list(client["gigachat"]["users"].find({})))
